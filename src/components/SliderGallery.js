c409e3acfaccd8512223dc679cc6fa68fad19c28
ckodev <cowens17@my.bcit.ca>
2022-06-18 06:18:00 -0700

    day 4. Slider gallery & react icons.

--- src/components/SliderGallery.js
+++ src/components/SliderGallery.js
@@ -1,21 +1,55 @@
-import React from 'react'
+import React from 'react';
+import {useState} from 'react';
+import {FaArrowAltCircleRight, FaArrowAltCircleLeft } from 'react-icons/fa'
+
+import {Link} from 'react-router-dom'
+
 
 
 function SliderGallery({projectData}) {
 
+  const [current, setCurrent] = useState(0);
+  const length = projectData.length
+
+  const nextSlide = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1)
+  }
+
+  const prevSlide = () => {
+    setCurrent(current === 0 ? length -1 : current -1)
+  }
+
+  if (!Array.isArray(projectData) || projectData.length <= 0) {
+    return null;
+  }
 
 
-
-console.log(projectData)
+  function scrollToTop() {
+    window.scrollTo({top:0, behavior:'smooth'} );
+  }
+  console.log(current)
 
   return (
-    <>
-    <div className='more-projects-gallery'>
-      {projectData.map(data => <img className="image-slides" src={data._embedded['wp:featuredmedia'][0].media_details.sizes.thumbnail.source_url} alt={data._embedded['wp:featuredmedia'][0].alt_text} />)}
-    </div>
 
-    </>
+    <section className='slider'>
+   
+      <FaArrowAltCircleLeft className='arrow-left' onClick={prevSlide}/>
+        {projectData.map((slide, index) => {
+          return(
+
+            
+            <Link className={index === current ? "slide-active" : "slide"} key={index} onClick={scrollToTop} to={`/SingleProject/${slide.id}`}>
+            {index === current && (<img className="slider-image" src={slide._embedded['wp:featuredmedia'][0].media_details.sizes.thumbnail.source_url} alt={slide._embedded['wp:featuredmedia'][0].alt_text} />)}
+            </Link>
+          )
+        })}
+      <FaArrowAltCircleRight className='arrow-right' onClick={nextSlide}/>
+
+      
+    </section>
+   
   )
 }
 
 export default SliderGallery
+

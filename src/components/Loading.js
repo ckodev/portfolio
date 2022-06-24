a491f15656455c14c6c48dd398cdc2a636b6ae7f
ckodev <cowens17@my.bcit.ca>
2022-06-24 09:54:22 -0700

    changed Loading bar to new gif

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,14 +1,12 @@
-import loadingImg from "../images/lightning-loading.gif"
+import loadingImg from "../images/lightning-gif.gif"
 
 const Loading = () => {
     
     return (
-   
        
             <div className="loading-container">
                  <img src={loadingImg} alt="Loading" className="loading" id="loading" />
             </div>
-      
       
     )
 

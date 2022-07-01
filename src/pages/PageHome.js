35bad46ec40250c8b354de7dc47954ccac617415
ckodev <cowens17@my.bcit.ca>
2022-07-01 07:34:02 -0700

    work on home page animation

--- src/pages/PageHome.js
+++ src/pages/PageHome.js
@@ -1,50 +1,30 @@
 import React from 'react'
-import { useState, useEffect } from 'react'
-import Loading from '../components/Loading'
+
+
+import MustacheLeft from '../svgs/MustacheLeft'
+import MustacheRight from '../svgs/MustacheRight'
+import Cko from '../svgs/Cko'
+import Dev from '../svgs/Dev'
+import Dot from '../svgs/Dot'
+import EyeBrowLeft from '../svgs/EyeBrowLeft'
+import EyeBrowRight from '../svgs/EyeBrowRight'
+
 
 function PageHome() {
-
-
-  const [isLoaded, setLoaded] = useState(false)
-
-  
-  const restPath = 'http://ckodev.com/ckodev/wp-json/wp/v2/pages/98?_embed';
-  const [restData, setData] = useState([])
-  
-
-  useEffect(() => {
-      const fetchData = async () => {
-          const response = await fetch(restPath)
-          if ( response.ok ) {
-              const data = await response.json()
-              setData(data)
-              setLoaded(true)
-          } else {
-              setLoaded(false)
-          }
-      }
-      fetchData()
-  }, [])
 
 
 
   return (
     <>
-
-
-      { isLoaded ?
-
-        <div className='entry-content'>
-            <section className='landing' id='landing'>
-                <h2>{restData.id}</h2>
-            </section>
-        </div>
-
-      : 
-        <Loading />
-      }
-
-
+            <div className="logo-container">
+              <MustacheLeft />
+              <MustacheRight />
+              <Cko/>
+              <Dev/>
+              <Dot/>
+              <EyeBrowLeft/>
+              <EyeBrowRight/>
+            </div>
     </>
   )
 }

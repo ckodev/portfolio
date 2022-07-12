925cfc3d317a039c15817bccb6d852e070118b6d
ckodev <cowens17@my.bcit.ca>
2022-07-12 07:09:12 -0700

    change favicon

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,11 +1,15 @@
-import loadingImg from "../images/lightning-gif.gif"
+// import loadingImg from "../images/lightning-gif.gif"
 
 const Loading = () => {
     
     return (
        
             <div className="loading-container">
-                 <img src={loadingImg} alt="Loading" className="loading" id="loading" />
+               
+                <svg xmlns="http://www.w3.org/2000/svg" width="22.308" height="28.931" viewBox="0 0 22.308 28.931">
+                    <path id="Icon_weather-lightning" data-name="Icon weather-lightning" d="M9.552,35.023h.586L20.469,19.736q.2-.405-.225-.405H15.979l4.49-8.184c.135-.27.03-.405-.3-.405H14.447a.545.545,0,0,0-.435.27L9.837,22.123c-.03.27.06.405.285.405h4.13Zm12.764-9.505h.405L30.56,14a.342.342,0,0,0,.06-.3c-.03-.075-.12-.105-.255-.105H27.211l3.274-6.052q.27-.45-.27-.45H26.1a.484.484,0,0,0-.45.285l-3.123,8.229a.324.324,0,0,0,.015.315.334.334,0,0,0,.285.105H25.89Z" transform="translate(-8.83 -6.592)" fill="none" stroke="#000" stroke-width="1"/>
+                </svg>
+                 
             </div>
       
     )
--- src/components/Page404.js
+++ src/components/Page404.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import {FaThumbsDown, } from 'react-icons/fa';
 
 function Page404() {
   return (
-    <div>Page404 - Do something else</div>
+    <div className='error-page'>
+
+      <p>404 - This page does not exit...  exist </p>
+      <FaThumbsDown/>
+
+    </div>
   )
 }
 
